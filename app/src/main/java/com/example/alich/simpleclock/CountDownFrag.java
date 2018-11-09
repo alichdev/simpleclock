a155fe128f67538e8c572032b0f807e857a5bb2b
Ali Chitsazian <alichitsazian@hotmail.com>
2018-11-10 00:04:33 +0330

    write and desiegn views

---
+++ app/src/main/java/com/example/alich/simpleclock/CountDownFrag.java
@@ -0,0 +1,21 @@
+package com.example.alich.simpleclock;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alich on 11/9/18.
+ */
+
+public class CountDownFrag extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.count_down_fragment ,container ,false);
+    }
+}
---
+++ app/src/main/java/com/example/alich/simpleclock/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.alich.simpleclock;
+
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button stopWatch;
+    Button countDown;
+
+    StopWatchFrag stopWatchFrag = new StopWatchFrag();
+    CountDownFrag countDownFrag = new CountDownFrag();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+        configure();
+
+
+
+    }
+
+    private void configure() {
+//        getFragmentManager().beginTransaction()
+//                .add(R.id.main_frame ,stopWatchFrag)
+//                .commit();
+
+
+        stopWatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//
+            }
+        });
+        countDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                
+            }
+        });
+
+    }
+
+    private void findViews(){
+        stopWatch = (Button) findViewById(R.id.stop_watch_button);
+        countDown = (Button) findViewById(R.id.count_down_button);
+
+    }
+}
